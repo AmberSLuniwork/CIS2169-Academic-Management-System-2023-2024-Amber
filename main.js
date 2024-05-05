82e701b50e5aa067f60c3eb549ff483487e9b62a
Amber <25276620@edgehill.ac.uk>
2024-05-05 18:01:15 +0100

    Added display and hide functionality to modules. Testing to see if all modules are being loaded correctly.

--- main.js
+++ main.js
@@ -1,58 +1,96 @@
 var pageCounter = 1;
-var moduleContainer = document.getElementById('module-container');
-var btn = document.getElementById("course-btn");
+var courseBtn = document.getElementById("course-btn");
+var scheduleBtn = document.getElementById("schedule-btn");
+var academicBtn = document.getElementById("academic-btn");
+var article = document.getElementsByTagName("article")[0];
 
+//stops the view more courses button loading more pages than it should!
+var courseFirstPageLoaded = false;
 //I want to have a view more modules button. I think it makes more sense.
 var viewmorebtn = document.createElement("button");
 //styling the button.
 viewmorebtn.classList.add("btn");
 viewmorebtn.textContent = "See More Modules";
 
-
-btn.addEventListener("click", function(){
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://raw.githubusercontent.com/Edge-Hill-University-Web/CIS2169-Academic-Management-System-2023-2024/main/module-'+ pageCounter +'.json');
-  ourRequest.onload = function(){
-    //console.log(ourRequest.responseText);
-    //alert("You clicked the course button")
-    var ourData = JSON.parse(ourRequest.responseText);
-    //console.log(ourData[0]);
-    renderHTML(ourData);
-
-    //making it so you can hide the courses and click a different button.
-    if (btn.textContent === "View Courses"){
-      btn.textContent = "Hide courses";
-      moduleContainer.style.display = "block";
-    } else {
-      btn.textContent = "View Courses";
-      moduleContainer.style.display = "none";
-    }
-  };
-  ourRequest.send();
-});
+//creating a div to store the modules
+var moduleContainer = document.createElement("div");
+moduleContainer.classList.add("container");
+moduleContainer.setAttribute('id', 'module-container')
+article.appendChild(moduleContainer);
 
 viewmorebtn.addEventListener("click", function() {
   var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://raw.githubusercontent.com/Edge-Hill-University-Web/CIS2169-Academic-Management-System-2023-2024/main/module-'+ pageCounter +'.json');
+  ourRequest.open('GET', 'https://raw.githubusercontent.com/AmberSLuniwork/CIS2169-Academic-Management-System-2023-2024-Amber/main/module-'+ pageCounter +'.json');
   ourRequest.onload = function(){
     var ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
+    // Check if data is empty (i.e., module does not exist)
+    if (ourData.length === 0) {
+      alert("No more modules.");
+      viewmorebtn.style.display = 'none'; // Hide the button
+    } else {
+      renderHTML(ourData);
+    }
   };
   ourRequest.send();
   pageCounter++;
-  if (pageCounter > 3){
-    alert("No more modules.");
+});
+
+courseBtn.addEventListener("click", function(){
+  if (courseFirstPageLoaded === false && pageCounter === 1) {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', 'https://raw.githubusercontent.com/AmberSLuniwork/CIS2169-Academic-Management-System-2023-2024-Amber/main/module-1.json');
+    ourRequest.onload = function(){
+      var ourData = JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+      pageCounter++;
+      courseFirstPageLoaded = true;
+
+      // Toggle the display of module container and view more button
+      if (courseBtn.textContent === "View Courses"){
+        courseBtn.textContent = "Hide courses";
+        moduleContainer.style.display = "block";
+        viewmorebtn.style.display = "block"; // Show the view more button
+      } else {
+        courseBtn.textContent = "View Courses";
+        moduleContainer.style.display = "none";
+        viewmorebtn.style.display = "none"; // Hide the view more button
+        if (moduleContainer && moduleContainer.parentNode) {
+          moduleContainer.parentNode.removeChild(moduleContainer);
+        }
+        //resets page counter.
+        pageCounter = 1;
+        courseFirstPageLoaded = false; // fixed variable declaration
+      }
+    };
+    ourRequest.send();
+  } else {
+    // Toggle the display of module container and view more button
+    if (courseBtn.textContent === "View Courses"){
+      courseBtn.textContent = "Hide courses";
+      moduleContainer.style.display = "block";
+      viewmorebtn.style.display = "block"; // Show the view more button
+    } else {
+      courseBtn.textContent = "View Courses";
+      moduleContainer.style.display = "none";
+      viewmorebtn.style.display = "none"; // Hide the view more button
+      moduleContainer.innerHTML = ""; // Clear the content inside moduleContainer
+      //resets page counter.
+      pageCounter = 1;
+      courseFirstPageLoaded = false;
+    }
   }
 });
 
 //this is where it loads all the module data. I want it to match what I outlined in my sketches.
 function renderHTML(data){
+  //empty variable before the loop is added...
   var htmlString = "";
 //this is a for loop that loads all the data in one module.
   for(i = 0; i < data.length; i++){
     //put the course name front and center. Give it a h2 tag for screenreaders.
     htmlString +="<h2>"+data[i].Course+"</h2>";
     htmlString +="<p>" + "For " +data[i].Name + " students" + "</p>";
+    htmlString += "<p>" + "This module is " +data[i].credits + " and " +data[i].hours + " hours." + "</p>"
     htmlString +="<h3>" + "Assessments" + "</h3>";
     //starting a list here because I think having the weights and volumes
     //with the corresponding assessments makes more sense than having them
@@ -66,7 +104,7 @@ function renderHTML(data){
       // List inside list = multi level list! No styling required.
       htmlString += "<ul>"; 
       htmlString += "<li>";
-      htmlString += "<p>" + "Weight: " + data[i].Module.Weights[ii] + "</p>"; // Add weighting
+      htmlString += "<p>" + "Weight: " + data[i].Module.weights[ii] + "</p>"; // Add weighting
       htmlString += "</li>";
       htmlString += "<ul> <li> <p>" + "Volume: " + data[i].Module.Volume[ii] + "</p> </li> </ul>"
       htmlString += "</ul> </li>"; // End the weightings list + assessment item
